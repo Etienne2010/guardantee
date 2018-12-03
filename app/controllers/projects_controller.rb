05a87a3a57872a6c0f40cc743c0c4f41205a7fcd
Etienne Lioret <kavatine99@gmail.com>
2018-12-03 15:01:33 -0500

    Ajout des images

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -5,10 +5,30 @@ class ProjectsController < ApplicationController
     @projects = Project.all
   end
 
+  def show
+    @project = Project.find(params[:user_id])
+  end
+
   def new
+    @project = Project.new
+    @user = current_user
   end
 
   def create
+    @project = Project.new(proj_params)
+    @project.user = current_user
+    if @project.save
+      p "Project saved"
+      redirect_to user_project_path(current_user, @project)
+    else
+      p @project.errors
+      p "Project not saved"
+    end
+  end
 
+  private
+
+  def proj_params
+    params.require(:project).permit(:images => [])
   end
 end
