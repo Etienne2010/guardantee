3b3610745a81e9bf95ee2df1eadea09b3c191470
Etienne Lioret <kavatine99@gmail.com>
2018-12-06 13:15:44 -0500

    JS bugué

--- app/controllers/guarantees_controller.rb
+++ app/controllers/guarantees_controller.rb
@@ -1,4 +1,24 @@
 class GuaranteesController < ApplicationController
-  def index
+  before_action :authenticate_user!
+
+  def create
+    puts "create"
+    pledge_id = guarantees_params[:pledge_id]
+    pledge = Pledge.find(pledge_id)
+    project = Project.find(pledge.project)
+    if project.user_id == current_user.id
+      pledge.status = "refunded_by_investor"
+      pledge.save
+    end
+  end
+
+  private
+
+  def skip_pundit?
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+  end
+
+  def guarantees_params
+    params.permit(:pledge_id)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
     resources :projects
   end
   resources :pledges
+  resources :guarantees, only: :create
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
